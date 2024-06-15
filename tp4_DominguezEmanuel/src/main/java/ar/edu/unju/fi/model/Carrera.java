bc0aece4119d82007cd4649372e984f8c4a19a70
danip <danip@DESKTOP-TTAUVM8>
2024-06-15 14:39:53 -0300

    se refactorizan clases del paquete model para poder usar lombok | segundo punto

--- tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -2,6 +2,16 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 @Component
 public class Carrera {
 
@@ -21,44 +31,6 @@ public class Carrera {
 		this.estado = estado;
 	}
 
-	public int getCodigo() {
-		return codigo;
-	}
 
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public byte getCantidadAnios() {
-		return cantidadAnios;
-	}
-
-	public void setCantidadAnios(byte cantidadAnios) {
-		this.cantidadAnios = cantidadAnios;
-	}
-
-	public Boolean getEstado() {
-		return estado;
-	}
-
-	public void setEstado(Boolean estado) {
-		this.estado = estado;
-	}
-
-	//No necesario pero se puede usar para prueba
-	@Override
-	public String toString() {
-		return "Carrera [codigo=" + codigo + ", nombre=" + nombre + ", cantidadAnios=" + cantidadAnios + ", estado="
-				+ estado + "]";
-	}
-	
 	
 }
--- tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -4,6 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+
 @Component
 public class Materia {
 
@@ -18,77 +29,7 @@ public class Materia {
 	
 	@Autowired
 	private Carrera carrera;
-	
-	public Materia() {
-		
-	}
 
-	public Materia(int codigo, String nombre, String curso, byte cantidadHoras, String modalidad, Docente docente,
-			Carrera carrera) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.curso = curso;
-		this.cantidadHoras = cantidadHoras;
-		this.modalidad = modalidad;
-		this.docente = docente;
-		this.carrera = carrera;
-	}
-
-	public int getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getCurso() {
-		return curso;
-	}
-
-	public void setCurso(String curso) {
-		this.curso = curso;
-	}
-
-	public byte getCantidadHoras() {
-		return cantidadHoras;
-	}
-
-	public void setCantidadHoras(byte cantidadHoras) {
-		this.cantidadHoras = cantidadHoras;
-	}
-
-	public String getModalidad() {
-		return modalidad;
-	}
-
-	public void setModalidad(String modalidad) {
-		this.modalidad = modalidad;
-	}
-
-	public Docente getDocente() {
-		return docente;
-	}
-
-	public void setDocente(Docente docente) {
-		this.docente = docente;
-	}
-
-	public Carrera getCarrera() {
-		return carrera;
-	}
-
-	public void setCarrera(Carrera carrera) {
-		this.carrera = carrera;
-	}
 	
 	
 }
